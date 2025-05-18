4d8218f6ecb17063d0152b3e3ddb013b69198991
Limon salvaje <davidjosuemamaniparillo@gmail.com>
2025-05-18 09:32:00 -0500

    Ejercicio 2 de punteros

---
+++ Punteros_ejercicio_2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void numero_primo(int*);
+
+int main(){
+    int a;
+    cout<<"ingres el numero:\n";cin>>a;
+    numero_primo(&a);
+
+    return 0;
+
+}
+
+void numero_primo(int* a){
+    char band='V';
+    for(int i=2;i<*a;i++){
+        if(*a%i==0){
+            band='F';
+        }
+    }
+    if(band=='V'){
+        cout<<"el numero es primo\n";
+        cout<<"su dirección de memoria es: "<<a<<endl;
+    }else{
+        cout<<"el numero no es primo\n";
+        cout<<"su dirección de memoria es: "<<a<<endl;
+    }
+
+}
